48a7fd037e4b50e5783161b5acc9be6ba862ee85
mzxc <hao474798383@163.com>
2019-07-30 17:00:37 +0800

    分块下载维护

--- src/main/java/com/gomyck/fastdfs/starter/controller/ChunkDownloadHandler.java
+++ src/main/java/com/gomyck/fastdfs/starter/controller/ChunkDownloadHandler.java
@@ -49,20 +49,19 @@ public class ChunkDownloadHandler {
         long offset = 0L; //当前偏移量
         long downloadFileSize = chunkFileSize; //当前实际要下载的块大小
         long remoteFileSize = remoteFileInfo.getFileSize(); //文件服务器存储的文件大小 (byte为单位)
+        //todo 如果文件大小 小于分块大小, 一次性下载
+        if(remoteFileSize <= chunkFileSize){
+            byte[] content = ffsc.downloadFile(fileInfo.getGroup(), fileInfo.getUploadPath(), 0, remoteFileSize, callback);
+            ResponseWriter.writeFile(content, fileInfo.getName(), fileInfo.getType(), true);
+            return;
+        }
         for(;;cycle = cycle + 1L){
-            //todo 如果文件大小 小于分块大小, 一次性下载
-            if(remoteFileSize <= chunkFileSize){
-                byte[] content = ffsc.downloadFile(fileInfo.getGroup(), fileInfo.getUploadPath(), 0, remoteFileSize, callback);
-                ResponseWriter.writeFile(content, fileInfo.getName(), fileInfo.getType(), true);
-                return;
-            }
-
-            if((cycle + 1) * chunkFileSize < remoteFileInfo.getFileSize()){
+            if((cycle + 1) * chunkFileSize < remoteFileSize){
                 byte[] content = ffsc.downloadFile(fileInfo.getGroup(), fileInfo.getUploadPath(), offset, downloadFileSize, callback);
                 boolean ifDownload = ResponseWriter.writeFile(content, fileInfo.getName(), fileInfo.getType(), false);
                 if(!ifDownload) return;
             }else{
-                downloadFileSize = remoteFileInfo.getFileSize() - (cycle * chunkFileSize);
+                downloadFileSize = remoteFileSize - (cycle * chunkFileSize);
                 byte[] content = ffsc.downloadFile(fileInfo.getGroup(), fileInfo.getUploadPath(), offset, downloadFileSize, callback);
                 boolean ifDownload = ResponseWriter.writeFile(content, fileInfo.getName(), fileInfo.getType(), true);
                 if(!ifDownload) return;
