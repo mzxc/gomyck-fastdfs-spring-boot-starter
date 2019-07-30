803f4a34599b81bb48b70dbb1c3e4577d8499d16
mzxc <hao474798383@163.com>
2019-07-30 16:41:42 +0800

    分块下载维护

---
+++ src/main/java/com/gomyck/fastdfs/starter/common/FileNotFoundException.java
@@ -0,0 +1,10 @@
+package com.gomyck.fastdfs.starter.common;
+
+/**
+ * @author 郝洋 QQ:474798383
+ * @version [版本号/1.0]
+ * @see [相关类/方法]
+ * @since [2019-07-30]
+ */
+public class FileNotFoundException extends RuntimeException {
+}
