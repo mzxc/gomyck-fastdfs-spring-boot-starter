478538e664d2b5d02c4000dc35810b49e14f48ea
mzxc <hao474798383@163.com>
2019-07-30 17:23:48 +0800

    分块下载维护

--- src/main/java/com/gomyck/fastdfs/starter/controller/SimpleFileDownloadHandler.java
+++ src/main/java/com/gomyck/fastdfs/starter/controller/SimpleFileDownloadHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * @author 郝洋 QQ:474798383
+ * @author gomyck QQ:474798383
  * @version [版本号/1.0]
  * @see [相关类/方法]
  * @since [2019-07-30]
