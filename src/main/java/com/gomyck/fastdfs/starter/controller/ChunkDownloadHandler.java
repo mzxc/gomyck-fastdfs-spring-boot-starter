d3879db2780865a15e5d036fbfed0fc5d85fb1a7
mzxc <hao474798383@163.com>
2019-07-31 14:14:32 +0800

    修复一个小bug

--- src/main/java/com/gomyck/fastdfs/starter/controller/ChunkDownloadHandler.java
+++ src/main/java/com/gomyck/fastdfs/starter/controller/ChunkDownloadHandler.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author gomyck QQ:474798383
