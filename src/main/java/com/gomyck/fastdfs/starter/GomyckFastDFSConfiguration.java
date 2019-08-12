2be7c47fa0b56c7c895f8e64947393ede3505b8f
mzxc <hao474798383@163.com>
2019-08-12 10:14:45 +0800

    修改类加载方式

--- src/main/java/com/gomyck/fastdfs/starter/GomyckFastDFSConfiguration.java
+++ src/main/java/com/gomyck/fastdfs/starter/GomyckFastDFSConfiguration.java
@@ -12,7 +12,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
