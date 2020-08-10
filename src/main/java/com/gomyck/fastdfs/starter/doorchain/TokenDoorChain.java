7c92f88c62dab6fd6a1769d9526de24635c29b5d
mzxc <hao474798383@163.com>
2020-08-11 07:56:08 +0800

    新增防盗链, 具体逻辑待实现

---
+++ src/main/java/com/gomyck/fastdfs/starter/doorchain/TokenDoorChain.java
@@ -0,0 +1,17 @@
+package com.gomyck.fastdfs.starter.doorchain;
+
+/**
+ * @author gomyck
+ * @version 1.0.0
+ * @contact qq: 474798383
+ * @blog https://blog.gomyck.com
+ * @since 2020/8/11
+ */
+public class TokenDoorChain implements CkDoorChain {
+
+    @Override
+    public boolean pass() {
+        return false;
+    }
+
+}
