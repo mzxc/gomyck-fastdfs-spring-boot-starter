ce6099ab67a72e57fcff8300fa8dec24a0e5ff89
mzxc <hao474798383@163.com>
2020-09-30 13:29:00 +0800

    增加了 yml 提示

--- src/main/java/com/gomyck/fastdfs/starter/profile/FileServerProfile.java
+++ src/main/java/com/gomyck/fastdfs/starter/profile/FileServerProfile.java
@@ -16,24 +16,44 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @Data
 public class FileServerProfile {
 
+    /**
+     * 文件服务预览时 使用协议
+     */
     private String fileServerProtocol;
 
+    /**
+     * 文件服务地址 uri
+     */
     private String fileServerUrl;
 
+    /**
+     * 默认上传文件服务分组
+     */
     private String groupId = "group1";
 
+    /**
+     * 下载时块大小(byte 单位)
+     */
     private long downloadChunkSize = 1024 * 1024; //下载速度 byte
 
-    //最大下载文件数量
+    /**
+     * 最大下载文件数量
+     */
     private Integer maxDownloadFileNum = 100;
 
-    //分块大小 MB
+    /**
+     * 分块大小 (MB 单位)
+     */
     private Long chunkSize = 5L;
 
-    //是否开启异常处理增强
+    /**
+     * 是否开启异常处理增强
+     */
     private boolean enableErrorAdvice = false;
 
-    //错误页 hostname, 一般来说, 就是当前的项目 hostname + port, 但是在有网关的情况下, 需要配置成网关, 或者代理服务的地址, 否则会出现被墙的问题
+    /**
+     * 错误页 hostname, 一般来说, 就是当前的项目 hostname + port, 但是在有网关的情况下, 需要配置成网关, 或者代理服务的地址, 否则会出现被墙的问题
+     */
     private String errorPageHostName = "";
 
     public String getFileServerURI(){
