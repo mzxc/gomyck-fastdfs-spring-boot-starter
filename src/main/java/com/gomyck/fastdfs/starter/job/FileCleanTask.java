830943ddfa4cb0cb454d6d58a98b54a07e551a3a
mzxc <hao474798383@163.com>
2021-06-29 17:00:21 +0800

    validate

--- src/main/java/com/gomyck/fastdfs/starter/job/FileCleanTask.java
+++ src/main/java/com/gomyck/fastdfs/starter/job/FileCleanTask.java
@@ -36,8 +36,6 @@ import com.gomyck.util.CkDateUtil;
 import com.gomyck.util.R;
 import com.gomyck.util.StringJudge;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -52,8 +50,13 @@ import java.util.stream.Collectors;
  * 清理过期的文件(只针对加入了过期标志的文件信息)
  *
  * @author gomyck
- * @version 1.0.0
- * @since 2020-04-15
+ * --------------------------------
+ * | qq: 474798383                 |
+ * | email: hao474798383@163.com   |
+ * | blog: https://blog.gomyck.com |
+ * --------------------------------
+ * @version [1.0.0]
+ * @since 2021/6/29
  */
 @Component
 @Slf4j
