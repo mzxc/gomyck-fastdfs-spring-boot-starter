dea1442da80c61a421120dfc3e57eb5619e98e5f
mzxc <hao474798383@163.com>
2021-07-20 16:13:43 +0800

    validate

--- src/main/java/com/gomyck/fastdfs/starter/GomyckFastDFSConfiguration.java
+++ src/main/java/com/gomyck/fastdfs/starter/GomyckFastDFSConfiguration.java
@@ -19,7 +19,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-
+/**
+ * starter
+ *
+ * @author gomyck
+ * --------------------------------
+ * | qq: 474798383                 |
+ * | email: hao474798383@163.com   |
+ * | blog: https://blog.gomyck.com |
+ * --------------------------------
+ * @version [1.0.0]
+ * @since 2021/7/20
+ */
 @Configuration
 @Import(FdfsClientConfig.class)
 @ComponentScan("com.gomyck.fastdfs.starter")
