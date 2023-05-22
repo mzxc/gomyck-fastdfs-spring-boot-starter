303b196205bf0b2623e23d28b1e1f33ba9224bbe
medness <hao47479838@gmail.com>
2023-05-22 10:02:37 +0800

    feat(新增一个防盗链实体): 新增一个防盗链实体
    新增一个防盗链实体

---
+++ src/main/java/com/gomyck/fastdfs/starter/doorchain/ChaimEntity.java
@@ -0,0 +1,13 @@
+package com.gomyck.fastdfs.starter.doorchain;
+
+public class ChaimEntity {
+
+    private String shareHost;
+
+    private String token;
+
+    private String fileUrl;
+
+    private String fileName;
+
+}
