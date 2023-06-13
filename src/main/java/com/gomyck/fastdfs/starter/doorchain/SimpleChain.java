3359e7941606f54ce649cfd2a6f52c875addd136
medness <hao47479838@gmail.com>
2023-06-13 10:47:32 +0800

    feat(日志修改): 日志修改
    日志修改

---
+++ src/main/java/com/gomyck/fastdfs/starter/doorchain/SimpleChain.java
@@ -0,0 +1,23 @@
+package com.gomyck.fastdfs.starter.doorchain;
+
+public class SimpleChain implements ExecuteChain, Interceptor, Token, Validator{
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public boolean intercepted() {
+        return false;
+    }
+
+    @Override
+    public boolean valid() {
+        return false;
+    }
+
+    @Override
+    public boolean verifyIt(Object token) {
+        return false;
+    }
+}
