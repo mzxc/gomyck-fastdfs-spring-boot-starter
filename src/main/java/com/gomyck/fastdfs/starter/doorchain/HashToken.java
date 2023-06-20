500691266cb81d1cfe2b0f6e42d6a9427eedc456
medness <hao47479838@gmail.com>
2023-06-20 16:40:06 +0800

    feat(日志修改): 日志修改
    日志修改

--- src/main/java/com/gomyck/fastdfs/starter/doorchain/HashToken.java
+++ src/main/java/com/gomyck/fastdfs/starter/doorchain/HashToken.java
@@ -1,36 +1,36 @@
-/*
- * Copyright (c) 2022. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
- * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
- * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
- * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
- * Vestibulum commodo. Ut rhoncus gravida arcu.
- */
-
-package com.gomyck.fastdfs.starter.doorchain;
-
-/**
- * hash token 防盗链
- *
- * @author gomyck
- * --------------------------------
- * | qq: 474798383                 |
- * | email: hao474798383@163.com   |
- * | blog: https://blog.gomyck.com |
- * --------------------------------
- * @version [gomyck-quickdev-1.0.0]
- * @since 2022/5/20 10:08
- */
-public class HashToken implements Token{
-
-
-    /**
-     * 验证 hash token 是否过期
-     *
-     * @return 是否过期
-     */
-    @Override
-    public boolean valid() {
-        return false;
-    }
-
-}
+///*
+// * Copyright (c) 2022. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+// * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+// * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+// * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+// * Vestibulum commodo. Ut rhoncus gravida arcu.
+// */
+//
+//package com.gomyck.fastdfs.starter.doorchain;
+//
+///**
+// * hash token 防盗链
+// *
+// * @author gomyck
+// * --------------------------------
+// * | qq: 474798383                 |
+// * | email: hao474798383@163.com   |
+// * | blog: https://blog.gomyck.com |
+// * --------------------------------
+// * @version [gomyck-quickdev-1.0.0]
+// * @since 2022/5/20 10:08
+// */
+//public class HashToken implements Token{
+//
+//
+//    /**
+//     * 验证 hash token 是否过期
+//     *
+//     * @return 是否过期
+//     */
+//    @Override
+//    public boolean valid() {
+//        return false;
+//    }
+//
+//}
