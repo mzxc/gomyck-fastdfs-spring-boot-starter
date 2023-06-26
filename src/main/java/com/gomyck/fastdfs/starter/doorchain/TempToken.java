894756166c8bda78b705ee881b7be7ef77838e85
medness <hao47479838@gmail.com>
2023-06-26 08:03:41 +0800

    feat(opts): opts
    opts

--- src/main/java/com/gomyck/fastdfs/starter/doorchain/TempToken.java
+++ src/main/java/com/gomyck/fastdfs/starter/doorchain/TempToken.java
@@ -1,23 +1,23 @@
-package com.gomyck.fastdfs.starter.doorchain;
-
-/**
- * 临时防盗链, 临时性授权
- *
- * @author gomyck
- * --------------------------------
- * | qq: 474798383                 |
- * | email: hao474798383@163.com   |
- * | blog: https://blog.gomyck.com |
- * --------------------------------
- * @version [1.0.0]
- * @since 2023/4/13
- */
-public class TempToken implements Token{
-
-
-    @Override
-    public boolean valid() {
-        return false;
-    }
-
-}
+//package com.gomyck.fastdfs.starter.doorchain;
+//
+///**
+// * 临时防盗链, 临时性授权
+// *
+// * @author gomyck
+// * --------------------------------
+// * | qq: 474798383                 |
+// * | email: hao474798383@163.com   |
+// * | blog: https://blog.gomyck.com |
+// * --------------------------------
+// * @version [1.0.0]
+// * @since 2023/4/13
+// */
+//public class TempToken implements Token{
+//
+//
+//    @Override
+//    public boolean valid() {
+//        return false;
+//    }
+//
+//}
