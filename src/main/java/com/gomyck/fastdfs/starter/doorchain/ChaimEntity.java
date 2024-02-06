2727022be2679804386591f74ef17c8d8dd2e601
medness <hao47479838@gmail.com>
2024-02-06 17:15:42 +0800

    create h2 datasource

--- src/main/java/com/gomyck/fastdfs/starter/doorchain/ChaimEntity.java
+++ src/main/java/com/gomyck/fastdfs/starter/doorchain/ChaimEntity.java
@@ -13,24 +13,12 @@ package com.gomyck.fastdfs.starter.doorchain;
  */
 public class ChaimEntity {
 
-    /**
-     * 分享主机
-     */
     private String shareHost;
 
-    /**
-     * 令牌
-     */
     private String token;
 
-    /**
-     * 文件路径
-     */
     private String fileUrl;
 
-    /**
-     * 文件名
-     */
     private String fileName;
 
 }
