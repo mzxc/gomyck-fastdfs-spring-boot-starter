607d5dbc528994fe400328db382a1ff3ca6094a7
medness <hao47479838@gmail.com>
2024-05-23 08:25:39 +0800

    perf(change package): change package
    change package for dynamic datasource

--- src/main/java/com/gomyck/fastdfs/starter/doorchain/TokenValidator.java
+++ src/main/java/com/gomyck/fastdfs/starter/doorchain/TokenValidator.java
@@ -1,42 +1,42 @@
-/*
- * Copyright (c) 2022. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
- * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
- * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
- * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
- * Vestibulum commodo. Ut rhoncus gravida arcu.
- */
-
-package com.gomyck.fastdfs.starter.doorchain;
-
-import com.gomyck.cache.redis.starter.core.redis.RedisCache;
-import com.gomyck.util.ObjectJudge;
-import org.springframework.beans.factory.annotation.Autowired;
-
-/**
- * token 防盗链
- *
- * @author gomyck
- * --------------------------------
- * | qq: 474798383                 |
- * | email: hao474798383@163.com   |
- * --------------------------------
- * @version [gomyck-quickdev-1.0.0]
- * @since 2022/5/20 10:09
- */
-public class TokenValidator implements Validator<String>{
-
-    @Autowired
-    RedisCache rs;
-
-    /**
-     * 验证方法
-     *
-     * @param token token
-     * @return 是否成功
-     */
-    @Override
-    public boolean verifyIt(final String token) {
-        return ObjectJudge.notNull(rs.get(token));
-    }
-
-}
+///*
+// * Copyright (c) 2022. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+// * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+// * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+// * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+// * Vestibulum commodo. Ut rhoncus gravida arcu.
+// */
+//
+//package com.gomyck.fastdfs.starter.doorchain;
+//
+//import com.gomyck.cache.redis.starter.core.redis.RedisCache;
+//import com.gomyck.util.ObjectJudge;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+///**
+// * token 防盗链
+// *
+// * @author gomyck
+// * --------------------------------
+// * | qq: 474798383                 |
+// * | email: hao474798383@163.com   |
+// * --------------------------------
+// * @version [gomyck-quickdev-1.0.0]
+// * @since 2022/5/20 10:09
+// */
+//public class TokenValidator implements Validator<String>{
+//
+//    @Autowired
+//    RedisCache rs;
+//
+//    /**
+//     * 验证方法
+//     *
+//     * @param token token
+//     * @return 是否成功
+//     */
+//    @Override
+//    public boolean verifyIt(final String token) {
+//        return ObjectJudge.notNull(rs.get(token));
+//    }
+//
+//}
