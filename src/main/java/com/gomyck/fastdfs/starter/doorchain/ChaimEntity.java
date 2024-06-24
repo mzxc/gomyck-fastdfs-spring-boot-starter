3f00b05355e7d872557b63862924c59a1257ace2
medness <hao47479838@gmail.com>
2024-06-24 09:08:13 +0800

    perf(fixed BUG): fixed BUG
    change package for dynamic datasource

--- src/main/java/com/gomyck/fastdfs/starter/doorchain/ChaimEntity.java
+++ src/main/java/com/gomyck/fastdfs/starter/doorchain/ChaimEntity.java
@@ -1,24 +1,24 @@
-//package com.gomyck.fastdfs.starter.doorchain;
-//
-///**
-// * 防盗链实体
-// *
-// * @author gomyck
-// * --------------------------------
-// * | qq: 474798383                 |
-// * | email: hao474798383@163.com   |
-// * --------------------------------
-// * @version [gomyck-quickdev-1.0.0]
-// * @since 2022/5/20 10:09
-// */
-//public class ChaimEntity {
-//
-//    private String shareHost;
-//
-//    private String token;
-//
-//    private String fileUrl;
-//
-//    private String fileName;
-//
-//}
+package com.gomyck.fastdfs.starter.doorchain;
+
+/**
+ * 防盗链实体
+ *
+ * @author gomyck
+ * --------------------------------
+ * | qq: 474798383                 |
+ * | email: hao474798383@163.com   |
+ * --------------------------------
+ * @version [gomyck-quickdev-1.0.0]
+ * @since 2022/5/20 10:09
+ */
+public class ChaimEntity {
+
+    private String shareHost;
+
+    private String token;
+
+    private String fileUrl;
+
+    private String fileName;
+
+}
--- src/main/java/com/gomyck/fastdfs/starter/doorchain/ExecuteChain.java
+++ src/main/java/com/gomyck/fastdfs/starter/doorchain/ExecuteChain.java
@@ -1,25 +1,25 @@
-///*
-// * Copyright (c) 2022. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-// * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
-// * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
-// * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
-// * Vestibulum commodo. Ut rhoncus gravida arcu.
-// */
-//
-//package com.gomyck.fastdfs.starter.doorchain;
-//
-///**
-// *
-// * 执行链
-// *
-// * @author gomyck
-// * --------------------------------
-// * | qq: 474798383                 |
-// * --------------------------------
-// * @since 2022/5/19 08:27
-// */
-//public interface ExecuteChain {
-//
-//    void execute();
-//
-//}
+/*
+ * Copyright (c) 2022. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.gomyck.fastdfs.starter.doorchain;
+
+/**
+ *
+ * 执行链
+ *
+ * @author gomyck
+ * --------------------------------
+ * | qq: 474798383                 |
+ * --------------------------------
+ * @since 2022/5/19 08:27
+ */
+public interface ExecuteChain {
+
+    void execute();
+
+}
